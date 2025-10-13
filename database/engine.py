9b1ded24073ce0f512a003290210b0ccb41ee967
alshansky-g <rgcs.rgcs@gmail.com>
2025-10-13 17:25:03 +0300

    migrate to postgresql

--- database/engine.py
+++ database/engine.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from database.models import Base
+from utils.config import config
 
-engine = create_async_engine("sqlite+aiosqlite:///pizza_db.db", echo=True)
+engine = create_async_engine(url=config.database_url, echo=True)
 session_maker = async_sessionmaker(bind=engine,
                                    class_=AsyncSession,
                                    expire_on_commit=False)
--- utils/config.py
+++ utils/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
+    database_url: str = ""
     bot_token: str = ""
     admins_list: list[int] = Field(default_factory=list)
 
