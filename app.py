cf60bd3fca7a608907a966a5da4548e3346894ed
alshansky-g <rgcs.rgcs@gmail.com>
2025-10-14 23:39:38 +0300

    add models banner, category, product, cart

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from handlers.admin_private import router as admin_router
 from handlers.user_group import router as user_group_router
 from handlers.user_private import router as user_private_router
 from middlewares.db import DataBaseSession
-from utils.bot_commands import commands
 from utils.config import config
 
 bot = Bot(token=config.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
@@ -24,10 +23,8 @@ dp.include_router(user_group_router)
 
 async def main():
     await create_db()
-    await bot.set_my_commands(commands)
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types(),
-                           admins_list=config.admins_list)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types(), admins_list=config.admins_list)
 
 
 if __name__ == "__main__":
--- database/models.py
+++ database/models.py
@@ -1,12 +1,26 @@
-from sqlalchemy import DateTime, Float, String, Text, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, Numeric, String, Text, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
-    updated_at: Mapped[DateTime] = mapped_column(
-        DateTime, server_default=func.now(), onupdate=func.now()
-    )
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+
+
+class Banner(Base):
+    __tablename__ = "banners"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(15), unique=True)
+    image: Mapped[str] = mapped_column(String(150), nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
 
 
 class Product(Base):
@@ -15,11 +29,36 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(150), nullable=False)
     description: Mapped[str] = mapped_column(Text)
-    price: Mapped[float] = mapped_column(Float(precision=2), nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
     image: Mapped[str] = mapped_column(String(150))
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id", ondelete="CASCADE"), nullable=False)
+
+    category: Mapped["Category"] = relationship(backref="product")
 
     def __repr__(self):
         return f"<Product id={self.id}, name={self.name}, price={self.price}>"
 
     def as_dict(self):
         return {key: value for key, value in self.__dict__.items() if not key.startswith("_")}
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(unique=True)
+    first_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    phone: Mapped[str] = mapped_column(String(13), nullable=True)
+
+
+class Cart(Base):
+    __tablename__ = "carts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id", ondelete="CASCADE"), nullable=False)
+    quantity: Mapped[int]
+
+    user: Mapped["User"] = relationship(backref="cart")
+    products: Mapped[list["Product"]] = relationship(backref="cart")
