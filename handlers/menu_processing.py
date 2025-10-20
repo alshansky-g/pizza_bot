68b21261909e00c96853c39d1107572cbda697fe
alshansky-g <rgcs.rgcs@gmail.com>
2025-10-21 02:52:10 +0300

    feat: add 2 level of inline menu

--- handlers/menu_processing.py
+++ handlers/menu_processing.py
@@ -1,8 +1,8 @@
 from aiogram.types import InputMediaPhoto
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.crud import orm_get_banner
-from keyboards.inline import get_user_main_btns
+from database.crud import orm_get_banner, orm_get_categories
+from keyboards.inline import get_user_catalog_btns, get_user_main_btns
 
 
 async def main_menu(session: AsyncSession, level: int, menu_name: str):
@@ -12,7 +12,17 @@ async def main_menu(session: AsyncSession, level: int, menu_name: str):
     return media, keyboard
 
 
+async def catalog(session: AsyncSession, level: int, menu_name: str):
+    banner = await orm_get_banner(session, menu_name)
+    image = InputMediaPhoto(media=banner.image, caption=banner.description)
+    categories = await orm_get_categories(session)
+    keyboard = get_user_catalog_btns(level=level, categories=categories)
+    return image, keyboard
+
+
 # TODO дописать
 async def get_menu_content(session: AsyncSession, level: int, menu_name: str):
     if level == 0:
         return await main_menu(session, level, menu_name)
+    elif level == 1:
+        return await catalog(session, level, menu_name)
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -3,6 +3,54 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
+class MenuCallback(CallbackData, prefix='menu'):
+    level: int
+    menu_name: str
+    category: int | None = None
+
+
+def get_user_main_btns(*, level: int, adjust_values: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+    buttons = {
+        'Товары 🍕': 'Категории',
+        'Корзина 🛒': 'Корзина',
+        'О нас ℹ️': 'О нас',
+        'Оплата 💳': 'Оплата',
+        'Доставка 🚗': 'Доставка'
+    }
+    for text, menu_name in buttons.items():
+        if menu_name == 'Категории':
+            keyboard.add(InlineKeyboardButton(
+                text=text, callback_data=MenuCallback(
+                    level=level + 1, menu_name=menu_name).pack()))
+        elif menu_name == 'Корзина':
+            keyboard.add(InlineKeyboardButton(
+                text=text, callback_data=MenuCallback(
+                    level=3, menu_name=menu_name).pack()))
+        else:
+            keyboard.add(InlineKeyboardButton(
+                text=text, callback_data=MenuCallback(
+                    level=level, menu_name=menu_name).pack()))
+
+    return keyboard.adjust(*adjust_values).as_markup()
+
+
+def get_user_catalog_btns(*, level: int, categories: list, adjust_values: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+    keyboard.add(InlineKeyboardButton(
+        text='Назад', callback_data=MenuCallback(
+            level=level - 1, menu_name='Главная').pack()))
+    keyboard.add(InlineKeyboardButton(
+        text='Корзина 🛒', callback_data=MenuCallback(
+            level=3, menu_name='Корзина').pack()))
+
+    for cat in categories:
+        keyboard.add(InlineKeyboardButton(
+            text=cat.name, callback_data=MenuCallback(
+                level=level + 1, menu_name=cat.name, category=cat.id).pack()))
+    return keyboard.adjust(*adjust_values).as_markup()
+
+
 def get_inline_kbd(
         *,
         buttons: dict[str, str],
@@ -13,37 +61,4 @@ def get_inline_kbd(
         keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
     if adjust_values:
         keyboard.adjust(*adjust_values)
-    return keyboard.as_markup()
-
-
-class MenuCallback(CallbackData, prefix='menu'):
-    level: int
-    menu_name: str
-
-
-def get_user_main_btns(*, level: int, adjust_values: tuple[int, ...] | None = (2,)):
-    keyboard = InlineKeyboardBuilder()
-    buttons = {
-        'Товары 🍕': 'catalog',
-        'Корзина 🛒': 'cart',
-        'О нас ℹ️': 'about',
-        'Оплата 💳': 'payment',
-        'Доставка 🚗': 'shipping'
-    }
-    for text, menu_name in buttons.items():
-        if menu_name == 'catalog':
-            keyboard.add(InlineKeyboardButton(
-                text=text, callback_data=MenuCallback(
-                    level=level + 1, menu_name=menu_name).pack()))
-        elif menu_name == 'cart':
-            keyboard.add(InlineKeyboardButton(
-                text=text, callback_data=MenuCallback(
-                    level=3, menu_name=menu_name).pack()))
-        else:
-            keyboard.add(InlineKeyboardButton(
-                text=text, callback_data=MenuCallback(
-                    level=level, menu_name=menu_name).pack()))
-    if adjust_values:
-        keyboard.adjust(*adjust_values)
-
     return keyboard.as_markup()
